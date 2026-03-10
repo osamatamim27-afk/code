7993c19453bffd91b117a80fcdf37db5a96277f8
osamatamim27-afk <osamatamim27@gmail.com>
2026-03-11 02:00:46 +0300

    Create NTT With Any Prime MOD.cpp

---
+++ NTT With Any Prime MOD.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void run_case(){
+    
+}
+
+int main(){
+    cin.tie(nullptr)->sync_with_stdio(false);
+    
+    
+    return 0;
+}
